fa5f7f45419cb381c1c272c625f3cb32b8279948
Priscila Pedrotti <priscila.pedrotti@outlook.com>
2022-12-01 15:52:39 -0300

    finished

---
+++ calculator.js
@@ -0,0 +1,69 @@
+function createCalculator() {
+  return {
+    display: document.querySelector(".display"),
+
+    start() {
+      this.clickButtons();
+      this.clickEnter();
+    },
+
+    clickEnter() {
+      this.display.addEventListener("keyup", (e) => {
+        if (e.keyCode === 13) {
+          this.result();
+        }
+      });
+    },
+
+  
+
+    clearDisplay() {
+      this.display.value = "";
+    },
+
+    delOne() {
+      this.display.value = this.display.value.slice(0, -1);
+    },
+
+    clickButtons() {
+      document.addEventListener("click", (e) => {
+        const el = e.target;
+
+        if (el.classList.contains("btn-num")) {
+          this.btnNum(el.innerText);
+        }
+        if (el.classList.contains("btn-clear")) {
+          this.clearDisplay();
+        }
+        if (el.classList.contains("btn-del")) {
+          this.delOne();
+        }
+        if (el.classList.contains("btn-eq")) {
+          this.result();
+        }
+      });
+    },
+
+    result() {
+     let count = this.display.value;
+      try {
+        count = eval(count);
+        if (!count) {
+          alert("Valor inválido");
+          return;
+        }
+        this.display.value = String(count);
+      } catch (e) {
+        alert("Valor inválido");
+        return;
+      }
+    },
+
+    btnNum(valor) {
+      this.display.value += valor;
+    },
+  };
+}
+
+const calculator = new createCalculator();
+calculator.start();
